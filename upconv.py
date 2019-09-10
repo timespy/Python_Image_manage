5bb55cd4dbac8cf4dc540635f8c88aeb21760f59
gaoyu <523351783@qq.com>
2019-09-10 01:59:17 -0500

    Add files via upload

---
+++ upconv.py
@@ -0,0 +1,50 @@
+import torch
+from torchvision import transforms
+import torchvision
+from PIL import Image
+import torch.nn as nn
+import time
+
+
+def extand(param):
+    C, H, W = param.shape
+    extant_w = torch.zeros((H, W*2))
+    extant_h = torch.zeros((H*2, W))
+    for i in range(H):
+        extant_w[i][2*i] = 1
+        extant_w[i][2*i+1] = 1
+    for i in range(W):
+        extant_h[2*i][i] = 1
+        extant_h[2*i+1][i] = 1
+    ret = torch.zeros((C, H*2, W*2))
+    for c in range(C):
+        channel = torch.mm(param[c], extant_w)
+        channel = torch.mm(extant_h, channel)
+        ret[c] = channel
+    return ret
+
+
+def main():
+    trans = transforms.Compose([transforms.ToTensor()])
+    img_path = './src/test/1.png'
+    img = Image.open(img_path).convert('RGB')
+    test = trans(img)
+
+    # test = torch.rand((3, 5, 5))
+    # print(test)
+
+    # test = torch.rand((3, 3))
+    # print(test)
+
+    print('start')
+    start = time.time()
+    res = extand(test)
+    stop = time.time()
+    torchvision.utils.save_image(res.data, './src/test/1_resize.png')
+    print("time : {}".format(stop - start))
+
+    # print(res)
+
+
+if __name__ == '__main__':
+    main()
